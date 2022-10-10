a46476886de3f5a5d527dab1082394ac4928fae4
Cameron Keif <cameron.keif@gmail.com>
2022-10-10 17:10:17 -0400

    get and delete by id

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Delete,
+  Patch,
+} from '@nestjs/common';
 import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
@@ -17,5 +25,24 @@ export class TasksController {
     @Body('description') description: string,
   ): Task {
     return this.tasksService.createTask(title, description);
+  }
+
+  @Get('/:id')
+  getTask(@Param('id') id) {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id) {
+    return this.tasksService.deleteTaskById(id);
+  }
+
+  @Patch('/:id')
+  updateTask(
+    @Param('id') id,
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ) {
+    return this.tasksService.updateTaskById(id, title, description);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { v4 as uuid } from 'uuid';
 import { Task, TaskStatus } from './task.model';
 
@@ -8,6 +8,44 @@ export class TasksService {
 
   getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  getTaskById(id: string): Task {
+    const task = this.tasks.find((task) => task.id === id);
+
+    if (!task) {
+      throw new HttpException('Task not found', HttpStatus.NOT_FOUND);
+    }
+
+    return task;
+  }
+
+  deleteTaskById(id: string) {
+    const task = this.tasks.find((task) => task.id === id);
+
+    if (!task) {
+      throw new HttpException('Task not found', HttpStatus.NOT_FOUND);
+    }
+
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+    throw new HttpException('', HttpStatus.NO_CONTENT);
+  }
+
+  updateTaskById(id: string, title: string, description: string): Task {
+    const taskIndex = this.tasks.findIndex((task) => task.id === id);
+
+    if (taskIndex === -1) {
+      throw new HttpException('Task not found', HttpStatus.NOT_FOUND);
+    }
+
+    const original = this.tasks[taskIndex];
+    const updated = {
+      ...this.tasks[taskIndex],
+      title: title || original.title,
+      description: description || original.description,
+    };
+    this.tasks[taskIndex] = updated;
+    return updated;
   }
 
   createTask(title: string, description: string): Task {
